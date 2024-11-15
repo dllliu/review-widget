14bcaa61f94dfcee2312be30d4896982d80c0bd6
dliu <dliu30@stuy.edu>
2024-11-15 15:06:35 -0500

    start of simple backend and frontend flask integration

--- backend/inference.py
+++ backend/inference.py
@@ -149,7 +149,7 @@ def categorize_question(question, categories):
 if __name__ == "__main__":
     urls= [
         "https://kith.com/collections/kith-footwear/products/x2j162xf85500",
-        "https://www.amazon.com/eos-Cashmere-Moisture-Lightweight-Non-Greasy/dp/B08KT2Z93D/?_encoding=UTF8&pd_rd_w=a3wu2&content-id=amzn1.sym.aeef70de-9e3e-4007-ae27-5dbb7b4a72f6&pf_rd_p=aeef70de-9e3e-4007-ae27-5dbb7b4a72f6&pf_rd_r=21NA7K6QEXX5X2F6N2KM&pd_rd_wg=l7cK3&pd_rd_r=903996a9-0eaa-4382-bfac-680c67cfe909&ref_=pd_hp_d_btf_crs_zg_bs_3760911",
+        "https://www.amazon.com/Olaplex-Bond-Smoother-3-3-Fl/dp/B07PW4MTHV/?_encoding=UTF8&pd_rd_w=Z2OcD&content-id=amzn1.sym.aeef70de-9e3e-4007-ae27-5dbb7b4a72f6&pf_rd_p=aeef70de-9e3e-4007-ae27-5dbb7b4a72f6&pf_rd_r=SWQMRM5QZDWCJRVAT6S5&pd_rd_wg=YHYmG&pd_rd_r=53f85db4-5bf3-48c3-b2e7-90db7883f0eb&ref_=pd_hp_d_btf_crs_zg_bs_3760911",
         #Captcha"https://www.walmart.com/ip/Austin-Peanut-Butter-on-Cheese-Sandwich-Crackers-Single-Serve-Snack-Crackers-20-Count/1837462801?classType=REGULAR&athbdg=L1600&adsRedirect=true",
         #Timeout"https://www.meijer.com/shopping/product/sony-zx-series-stereo-headphones-black/2724286708.html",
         "https://us.shein.com/SHEIN-EZwear-Women-s-Hooded-Sweatshirt-With-Slogan-Print-And-Kangaroo-Pocket-FAITH-OVER-FEAR-PSALM-563-p-29621449.html?src_identifier=uf=usbingsearch09_cheaptrendyclothes02_20220804&src_module=ads&mallCode=1&pageListType=4&imgRatio=3-4",
